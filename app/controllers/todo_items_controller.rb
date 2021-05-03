e4cd9a3f13680a938a5118f4cdafde699c60c064
ms2138 <m38371@gmail.com>
2021-05-02 18:18:16 -0700

    Add functionality to allow the creation of todo items.  Add _form.html.erb and _error_messages.html.erb"

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -5,6 +5,18 @@ class TodoItemsController < ApplicationController
   end
 
   def new
+    @todo_item = @todo_list.todo_items.new
+  end
+
+  def create
+    respond_to do |format|
+      @todo_item = @todo_list.todo_items.new(todo_item_params)
+      if @todo_item.save
+        format.html { redirect_to todo_list_todo_items_path, flash: { success: "Added todo item." } }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   def edit
