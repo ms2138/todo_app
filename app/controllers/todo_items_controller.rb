259cbbeb5f3b7dca9084157d913ecc99ffa24796
ms2138 <m38371@gmail.com>
2021-05-02 18:09:36 -0700

    Add set_todo_list, todo_item_params and before action call set todo list method to TodoItemsController

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,4 +1,6 @@
 class TodoItemsController < ApplicationController
+  before_action :set_todo_list
+
   def index
   end
 
@@ -7,4 +9,14 @@ class TodoItemsController < ApplicationController
 
   def edit
   end
+
+  private
+
+    def set_todo_list
+      @todo_list = TodoList.find(params[:todo_list_id])
+    end
+
+    def todo_item_params
+      params.require(:todo_item).permit(:content)
+    end
 end
