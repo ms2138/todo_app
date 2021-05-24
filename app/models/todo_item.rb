8b9425cacccef433843fad995c4acdfae943d400
ms2138 <m38371@gmail.com>
2021-05-24 12:05:30 -0700

    Add functionality to display complete and incomplete todo items on todo list show page

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -3,6 +3,9 @@ class TodoItem < ApplicationRecord
 
   validates :content, presence: true, length: { minimum: 10 }
 
+  scope :complete, -> { where("completed_at is not null") }
+  scope :incomplete, -> { where(completed_at: nil) }
+
   def completed?
     !completed_at.blank?
   end
--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -3,4 +3,12 @@ class TodoList < ApplicationRecord
 
   validates :title, presence: true, length: { minimum: 3 }
   validates :description, presence: true, length: { minimum: 10 }
+
+  def has_completed_items?
+    todo_items.complete.size > 0
+  end
+
+  def has_incomplete_items?
+    todo_items.incomplete.size > 0
+  end
 end
